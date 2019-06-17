ff37e61b25d5354850c144ab379d79f2e3cbe3ad
xiexiaoqing1980 <1805744514@qq.com>
2019-06-17 23:19:43 +0800

    commit redis configuration

---
+++ src/main/java/com/example/demo/config/CacheConfigure.java
@@ -0,0 +1,93 @@
+package com.example.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+@Configuration
+public class CacheConfigure extends CachingConfigurerSupport {
+    //自定义key的生成规则
+
+    @Bean
+        //如使用注解的话需要配置cacheManager
+    CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        //初始化一个RedisCacheWriter
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        //设置默认超过期时间是1天
+        defaultCacheConfig.entryTtl(Duration.ofDays(1));
+        //初始化RedisCacheManager
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
+        return cacheManager;
+    }
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator(){
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for(Object obj:params){
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param connectionFactory
+     * @return
+     */
+    // 以下两种redisTemplate自由根据场景选择
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(serializer);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        return stringRedisTemplate;
+    }
+
+
+
+}
--- src/test/java/com/example/demo/SpringDataApplicationTests.java
+++ src/test/java/com/example/demo/SpringDataApplicationTests.java
@@ -6,7 +6,9 @@ import com.example.demo.Excelprocess.ExcelListener;
 import com.example.demo.pojo.user;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
@@ -18,32 +20,11 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringDataApplicationTests {
+	@Autowired
+	StringRedisTemplate template;
 
 	@Test
 	public void contextLoads() {
 
-		InputStream inputStream = null;
-		ExcelListener excelListener = new ExcelListener();
-		List<Object> datas = null;
-
-		try {
-//            inputStream = file.getInputStream();
-			inputStream=new FileInputStream(new File("D://userdata.xlsx"));
-			ExcelReader reader = new ExcelReader(inputStream, user.class,excelListener);
-			reader.read(new Sheet(1, 2, null));
-			datas = excelListener.getDatas();
-		} catch (Exception e) {
-		} finally {
-			try {
-				inputStream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			System.out.println(datas);
-		}
-
-
 	}
-
 }
